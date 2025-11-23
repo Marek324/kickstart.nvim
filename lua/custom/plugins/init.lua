cc6b956cd6aaf56e31de0c0d817bae7243839859
Marek324 <mhric24@gmail.com>
2025-11-23 14:29:49 +0100

    add lazydocker

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -34,4 +34,23 @@ return {
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
+  {
+    'mgierada/lazydocker.nvim',
+    dependencies = { 'akinsho/toggleterm.nvim' },
+    config = function()
+      require('lazydocker').setup {
+        border = 'curved', -- valid options are "single" | "double" | "shadow" | "curved"
+      }
+    end,
+    event = 'BufRead',
+    keys = {
+      {
+        '<leader>ld',
+        function()
+          require('lazydocker').open()
+        end,
+        desc = 'Open Lazydocker floating window',
+      },
+    },
+  },
 }
